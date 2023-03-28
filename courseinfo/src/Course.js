b7354a7b40843fe82eae093e91f8593570eb5094
NaveenJsr <naveenjsr99@gmail.com>
2023-03-28 20:22:54 +0530

    Exercise 2.2: Course Information Step7 Complete

--- courseinfo/src/Course.js
+++ courseinfo/src/Course.js
@@ -3,12 +3,16 @@ import Content from "./Content"
 
 const Course = ( { course } ) =>
 {
+    const totalExercises = course.parts.reduce( ( sum, part ) => sum + part.exercises, 0 )
+
     return (
         <div>
             <Header course={ course } />
             <Content parts={ course.parts } />
+            <p><strong>total of { totalExercises } exercises</strong></p>
         </div>
     )
 }
+
 
 export default Course
