189ad4836996414cc2e94dcc31532f55cc8816df
NaveenJsr <naveenjsr99@gmail.com>
2023-05-24 21:36:44 +0530

    Exercise 2.12: The Phonebook step7 Complete

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,80 +1,87 @@
 import { useState, useEffect } from 'react'
-import Filter from './Filter'
-import PersonForm from './PersonForm'
-import Persons from './Persons'
+import Header from './Components/Header'
+import Search from './Components/Search'
+import AddContact from './Components/AddContact'
+import AllPersons from './Components/AllPersons'
 import axios from 'axios'
 
 const App = () =>
 {
-
-  const [ persons, setPersons ] = useState( [] )
+  const [ persons, setPersons ] = useState( [
+    { name: 'Arto Hellas', number: '040-123456' },
+  ] )
   const [ newName, setNewName ] = useState( '' )
-  const [ newNumber, setNewNumber ] = useState( '' )
-  const [ filter, setFilter ] = useState( '' )
+  const [ number, setNumber ] = useState( '' )
+  const [ search, setSearch ] = useState( '' )
+  const [ filterPersons, setFilterPersons ] = useState( [] )
+
+  const formHandler = ( e ) =>
+  {
+    e.preventDefault()
+    let allPersons = [ ...persons ]
+    let isPresent = false;
+    allPersons.forEach( ( p ) =>
+    {
+      if ( p.name === newName )
+      {
+        isPresent = true
+        alert( `${ newName } is already added to phonebook` )
+      }
+    } )
+
+    if ( isPresent )
+    {
+      return
+    }
+
+    axios.post( "http://localhost:3001/persons", { name: newName, number: number } )
+      .then( ( response ) =>
+      {
+        setPersons( [ ...persons, response.data ] )
+        setNewName( "" )
+        setNumber( "" )
+      } )
+  }
+
+  const searchHandler = ( e ) =>
+  {
+    let currInput = e.target.value
+    setSearch( currInput )
+
+    let allPersons = [ ...persons ]
+
+    let filterResult = allPersons.filter( ( p ) =>
+    {
+      return p.name.toLowerCase().includes( currInput.toLowerCase() )
+    } )
+
+    setFilterPersons( filterResult )
+
+
+  }
 
   useEffect( () =>
   {
-    axios
-      .get( 'http://localhost:3001/persons' )
+    axios.get( "http://localhost:3001/persons" )
       .then( response =>
       {
         setPersons( response.data )
-        console.log( response.data )
       } )
-
   }, [] )
-
-  const addPerson = ( event ) =>
-  {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1,
-    }
-    if ( persons.some( person => person.name === newName ) )
-    {
-      alert( `${ newName } is already added to phonebook` )
-      return
-    }
-    setPersons( persons.concat( personObject ) )
-    setNewName( '' )
-    setNewNumber( '' )
-  }
-
-  const handleNameChange = ( event ) =>
-  {
-    setNewName( event.target.value )
-  }
-
-  const handleNumberChange = ( event ) =>
-  {
-    setNewNumber( event.target.value )
-  }
-
-  const handleFilterChange = ( event ) =>
-  {
-    setFilter( event.target.value )
-  }
-
-  const personsToShow = filter
-    ? persons.filter( person => person.name.toLowerCase().includes( filter.toLowerCase() ) )
-    : persons
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter filter={ filter } handleFilterChange={ handleFilterChange } />
-      <h3>Add a new</h3>
-      <PersonForm
-        addPerson={ addPerson }
-        newName={ newName }
-        handleNameChange={ handleNameChange }
-        newNumber={ newNumber }
-        handleNumberChange={ handleNumberChange }
-      />
-      <h3>Numbers</h3>
-      <Persons persons={ personsToShow } />
+      <Header title="Phonebook" />
+      <Search search={ search } searchHandler={ searchHandler } />
+      <Header title="add a new" />
+      <AddContact formHandler={ formHandler } newName={ newName } setNewName={ setNewName } number={ number } setNumber={ setNumber } />
+      <Header title="Numbers" />
+      {
+        search && <AllPersons persons={ filterPersons } />
+      }
+      {
+        !search && <AllPersons persons={ persons } />
+      }
     </div>
   )
 }
---
+++ phonebook/src/services/ApiService.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const baseUrl = 'https://fullstack-phonebook-backend.onrender.com/api/persons'
+
+const getAll = ()=>{
+    const request  = axios.get(baseUrl)
+    return request.then(response=>response.data)
+}
+
+
+const savePhone = (contact)=>{
+    const request = axios.post(baseUrl,contact)
+    return request.then(response=>response.data) 
+}
+
+const deletePhone = (id)=>{
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request
+}
+
+const updatePhone = (contact)=>{
+    const request = axios.put(`${baseUrl}/${contact.id}`,contact)
+    return request.then(response=>response.data)
+}
+
+const ApiService =  {
+    getAll,
+    savePhone,
+    deletePhone,
+    updatePhone
+}
+
+export default ApiService
